053f408f91113800d5a481ae7e5b72808c2547c6
AyushInKC <sirfayush1008@gmail.com>
2025-06-05 00:30:33 +0530

    Project Setup

---
+++ src/main/java/com/JavaJunie/YogoTribeAssingment/YogoTribeAssingmentApplication.java
@@ -0,0 +1,13 @@
+package com.JavaJunie.YogoTribeAssingment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogoTribeAssingmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogoTribeAssingmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaJunie/YogoTribeAssingment/YogoTribeAssingmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaJunie.YogoTribeAssingment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogoTribeAssingmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
